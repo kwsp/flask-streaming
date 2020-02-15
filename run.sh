a52ede5f24f6d25025d4262c9a3e3834e1d73fdb
Tiger Nie <nhl0819@gmail.com>
2020-02-15 00:38:10 -0600

    centered img, use threads instead of gevent

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-CAMERA=opencv gunicorn --worker-class gevent --workers 1 --bind 0.0.0.0:5000 app:app
+#CAMERA=opencv gunicorn --worker-class gevent --workers 1 --bind 0.0.0.0:5000 app:app
+CAMERA=opencv gunicorn --threads 5 --workers 1 --bind 0.0.0.0:5000 app:app
